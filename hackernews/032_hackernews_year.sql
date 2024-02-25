6b93a0a1b06fa55820360e8c3328f02fc515f621
Duc Che <ducchetrongminh@gmail.com>
2024-02-25 18:43:29 +0700

    rename col

--- hackernews/032_hackernews_year.sql
+++ hackernews/032_hackernews_year.sql
@@ -1,6 +1,6 @@
 WITH summary_hackernews_user AS (
   SELECT 
-    DATE_TRUNC(first_posted_date, YEAR) AS year
+    DATE_TRUNC(first_posted_date, YEAR) AS start_of_year
     , COUNT(hackernews_user_id) AS new_users -- for user acquisition analysis
   FROM `vit-lam-data.cleansed.hackernews_user`
   GROUP BY 1 
@@ -9,8 +9,8 @@ WITH summary_hackernews_user AS (
 , calculate_metrics_1 AS (
   SELECT 
     *
-    , SUM(new_users) OVER (ORDER BY year ROWS BETWEEN UNBOUNDED PRECEDING AND 1 PRECEDING) AS total_users_start_of_year
-    , SUM(new_users) OVER (ORDER BY year) AS total_users_end_of_year
+    , SUM(new_users) OVER (ORDER BY start_of_year ROWS BETWEEN UNBOUNDED PRECEDING AND 1 PRECEDING) AS total_users_start_of_year
+    , SUM(new_users) OVER (ORDER BY start_of_year) AS total_users_end_of_year
   FROM summary_hackernews_user
 )
 
@@ -18,4 +18,4 @@ SELECT
   *
   , new_users / total_users_start_of_year AS user_acquisition_rate
 FROM calculate_metrics_1
-ORDER BY 1 
+ORDER BY 1
