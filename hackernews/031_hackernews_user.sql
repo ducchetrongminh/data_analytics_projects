80339a1d7d504996d83fb5ed6993b1885717df3d
Duc Che <ducchetrongminh@gmail.com>
2024-02-28 11:07:49 +0700

    add 1st/last story date

--- hackernews/031_hackernews_user.sql
+++ hackernews/031_hackernews_user.sql
@@ -2,6 +2,8 @@ SELECT
   hackernews_user_id
   , CAST(MIN(posted_at) AS DATE) AS first_posted_date -- for user acquisition analysis
   , CAST(MAX(posted_at) AS DATE) AS last_posted_date
+  , CAST(MIN(CASE WHEN type = 'story' THEN posted_at END) AS DATE) AS first_story_date
+  , CAST(MAX(CASE WHEN type = 'story' THEN posted_at END) AS DATE) AS last_story_date
 FROM `vit-lam-data.cleansed.hackernews`
 GROUP BY 1 
 ORDER BY 2
