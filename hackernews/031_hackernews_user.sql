4eef7019071e9e4934cec8d1e8491adee6daf484
Duc Che <ducchetrongminh@gmail.com>
2024-02-28 10:44:19 +0700

    add last_posted_date

--- hackernews/031_hackernews_user.sql
+++ hackernews/031_hackernews_user.sql
@@ -1,6 +1,7 @@
 SELECT 
   hackernews_user_id
   , CAST(MIN(posted_at) AS DATE) AS first_posted_date -- for user acquisition analysis
+  , CAST(MAX(posted_at) AS DATE) AS last_posted_date
 FROM `vit-lam-data.cleansed.hackernews`
 GROUP BY 1 
 ORDER BY 2
